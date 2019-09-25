b8322b13425e0426ff9b14618a75bfdde515fe56
nabenabe0258 <nabenabe0258@gmail.com>
2019-09-25 15:13:57 +0900

    mercariの作成

---
+++ src/main/java/com/example/MercariApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercariApplication.class, args);
+	}
+
+}
